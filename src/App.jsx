336036eaf9b43d265d441d4f801ee51ba33689e1
fairyAriZ <pashil364@gmail.com>
2026-06-17 10:53:20 +0530

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Root from './layouts/Root'
+import Landing from './pages/Landing'
+import Session from './pages/Session'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Landing />} />
+        <Route path="session/:id" element={<Session />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
+    )
+  )
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
